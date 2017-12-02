c928c58f9edb784883d1dfde3d759e46ab499cdb
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-02 11:05:29 +0100

    Añadido archivo app con sus correspondientes parser para JSON

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var mongoose = require('mongoose');
-
+var app = require('./app')
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mean_social', {useMongoClient: true})
         .then(() => {
