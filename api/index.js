d59240b2c5d4ee65c55b5b3e2e6b24fa5627657d
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-02 11:08:39 +0100

    Cambiado puerto por defecto y configurado

--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@
 
 var mongoose = require('mongoose');
 var app = require('./app')
+var port = 3800;
+// Conexión Database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mean_social', {useMongoClient: true})
         .then(() => {
-            console.log("La conexión se ha realizado con éxito")
+            console.log("La conexión se ha realizado con éxito");
+
+            // Crear servidor
+            app.listen(port, () => {
+                console.log("Servidor corriendo en http://localhost:" + port)
+            })
         })
         .catch(err => console.log(err))
