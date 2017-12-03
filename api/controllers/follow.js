f52f73cbe0d26a55571b19be33ca2f006b02531b
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-03 16:18:08 +0100

    Creado controlador y configuración básica de rutas

---
+++ api/controllers/follow.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var path = require('path');
+var fs = require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+
+function prueba(req, res) {
+    res.status(200).send({message: 'Hola mundo desde el controlador follows'});
+}
+
+module.exports = {
+    prueba
+}
