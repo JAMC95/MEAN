8c8ea7801f08da2f7a610ba3fd94ee252a45b09d
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-04 17:06:29 +0100

    Configuración inicial del controlador y router relacionado con las publicaciones

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 // cargar rutas
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
+var publication_router = require('./routes/publication');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended:false}))
@@ -18,6 +19,7 @@ app.use(bodyParser.json());
 // rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
+app.use('/api', publication_router);
 
 // exportar
 module.exports = app;
