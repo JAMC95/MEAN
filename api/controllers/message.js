1f1b6acd50a7f1ef9705643de8e82cc8dada60b5
Jose Antonio Moral <joxe.bailen@gmail.com>
2017-12-16 18:03:31 +0100

    Implementado método que lista los mensajes enviados por un usuario

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -52,7 +52,33 @@ function getRecivedMessages(req, res) {
     });
 
 }
+
+function getEmmitedMessages(req, res) {
+    var userId = req.user.sub;
+
+    var page = 1;
+    if(req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Message.find({emitter: userId}).populate('emitter reciver','name surname nick image _id').paginate(page, itemsPerPage, (err, messages,total) => {
+        if(err) if(!params.text || !params.reciver) return res.status(500).send({message: 'Error en la petición'});
+        if(!messages) return res.status(404).send({message: 'No hya mensajes'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            messages
+        })
+
+    });
+
+}
+
 module.exports = {
     saveMessage,
-    getRecivedMessages
+    getRecivedMessages,
+    getEmmitedMessages
 }
